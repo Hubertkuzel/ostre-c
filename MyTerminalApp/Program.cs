b4f83d9cf23b94bc0fde7558c90fa7c49a616cbc
jakub <jakub.artysiewicz@infotech.edu.pl>
2025-04-03 11:23:53 +0200

    odwoiwjop

--- MyTerminalApp/Program.cs
+++ MyTerminalApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Student 
+<student>list = new <student>list lista
+{
+    public string imie {get; set;}
+    static string nazwisko {get; set;}
+
+    public Student(string imie, string nazwisko){
+        imie = imie;
+        nazwisko = nazwisko;
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+    }
+    
+}
+List<student> Studenci = new List<student>();
