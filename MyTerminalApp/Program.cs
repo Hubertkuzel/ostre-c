3ab463a5507eec18d9b0f696d858f5113bd75f47
Hubertkuzel <hauek89@gmail.com>
2025-04-03 11:38:30 +0200

    dwkijwijdw

--- MyTerminalApp/Program.cs
+++ MyTerminalApp/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+class Student 
+<student>list = new <student>list lista
+{
+    public string imie {get; set;}
+    static string nazwisko {get; set;}
+
+    public Student(string imie, string nazwisko){
+        imie = imie;
+        nazwisko = nazwisko;
+    }
+
+}
+class Program
+{
+    static void Main()
+    {
+    }
+    
+}
+List<student> Studenci = new List<student>();
