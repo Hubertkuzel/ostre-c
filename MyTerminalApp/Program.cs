56dbacb3a7570f838133cb98fe31fd8d98aa4779
Gerard <gerard.dziwinski@infotech.edu.pl>
2025-04-03 12:44:09 +0200

    pare rzeczy

--- MyTerminalApp/Program.cs
+++ MyTerminalApp/Program.cs
@@ -5,13 +5,15 @@ class Student
 {
     public string imie {get; set;}
     static string nazwisko {get; set;}
+    private static int GradeAvg { get; set; }
 
-    public Student(string imie, string nazwisko){
+    public Student(string imie, string nazwisko, int Avg){
         imie = imie;
         nazwisko = nazwisko;
+        GradeAvg = Avg;
     }
-
 }
+
 class Program
 {
     static void Main()
@@ -21,15 +23,28 @@ class Program
 
         bool running = true;
         int decision = int.Parse(Console.ReadLine());
+        Console.WriteLine("1 - dodaj studenta\n2 - usuń studenta\n3 - wyszukiwanie studentów\n4 - obliczanie średniej ocen\n5 - wyłącz system");
         while (running)
         {
             switch (decision)
             {
                 case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Coś chyba poszło nie tak");
                     break;
             }
         }
     }
     
 }
-
